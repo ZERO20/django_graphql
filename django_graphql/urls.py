ba368222011868654a376f2993f7130cb3f4c2cb
Edgar de la Cruz <edgargdcv@gmail.com>
2022-12-08 18:27:26 -0600

    ref: added csrf_exempt to allow mutation

--- django_graphql/urls.py
+++ django_graphql/urls.py
@@ -18,8 +18,10 @@ from graphene_django.views import GraphQLView
 
 from django.contrib import admin
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql/', GraphQLView.as_view(graphiql=True)),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
