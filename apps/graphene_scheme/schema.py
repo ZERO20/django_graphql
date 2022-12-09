8038ceb95399dd9cf4a2ced6d1c3d2c0f5695c04
Edgar de la Cruz <edgargdcv@gmail.com>
2022-12-08 18:26:54 -0600

    feat: added mutation

--- apps/graphene_scheme/schema.py
+++ apps/graphene_scheme/schema.py
@@ -1,10 +1,15 @@
 import graphene
 
 from apps.graphene_scheme.query import Query as SchemeQuery
+from apps.graphene_scheme.mutation import Mutation as SchemeMutation
 
 
 class Query(SchemeQuery, graphene.ObjectType):
     pass
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(SchemeMutation, graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
